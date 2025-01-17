93002e0953ff4a3ba55bfe77d9f78a18b2f89047
JonathanManaka <mpomanaka91@gmail.com>
2025-01-17 12:32:09 +0200

    added colors

---
+++ styles/brandColors.js
@@ -0,0 +1,8 @@
+const brandColors = {
+    dark: "#001A83",
+    default: "#002DE3",
+    darkMode: "#375FFF",
+    light: "#879FFF",
+    background: "#D2D5F9"
+}
+export default brandColors
---
+++ styles/neutralColors.js
@@ -0,0 +1,11 @@
+const neutralColors = {
+    active: "#0F1828",
+    dark: "#152033",
+    body: "#1B2B48",
+    weak: "#A4A4A4",
+    disable: "#EDEDED",
+    line: "#EDEDED",
+    offWhite: "#F7F7FC",
+    white: "#FFFFFF"
+}
+export default neutralColors;
